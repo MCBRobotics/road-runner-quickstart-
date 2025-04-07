1e7d81aa00cef48813b1f39fe6ae0c61ac93fa63
MCB Robotics 21116 <162033832+MCBRobotics@users.noreply.github.com>
2025-04-07 15:34:36 +0100

    updated

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,8 @@
 package com.example.meepmeeptesting;
 
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -13,15 +15,26 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+
+
+
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(24, -64.25, Math.toRadians(270)))
+                        .setReversed(true)
+                        //.forward(25)
+                        //.strafeRight(36)
+                        //.turn(Math.toRadians(90))
+                        //.strafeRight(50) //finish
+                        .splineToLinearHeading(new Pose2d(3, -33, Math.toRadians(270)), Math.toRadians(90)) // park
+                        //.splineTo(new Vector2d(9, -10), 0)
+                        //.splineTo(new Vector2d(10, 15), 0)
+//                        .forward(30)
+//                        .turn(Math.toRadians(90))
+//                        .forward(30)
+//                        .turn(Math.toRadians(90))
+//                        .forward(30)
+//                        .turn(Math.toRadians(90))
+//                        .forward(30)
+                        //.turn(Math.toRadians(90))
                         .build());
 
 
